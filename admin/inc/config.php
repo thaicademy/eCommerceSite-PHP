a2444b51fcae97798a3d4ab9c6abc7d48e12b954
siamcodes <somkiat@gmail.com>
2022-12-13 19:33:22 +0700

    first commit

---
+++ admin/inc/config.php
@@ -0,0 +1,33 @@
+<?php
+// Error Reporting Turn On
+ini_set('error_reporting', E_ALL);
+
+// Setting up the time zone
+date_default_timezone_set('America/Los_Angeles');
+
+// Host Name
+$dbhost = 'localhost';
+
+// Database Name
+$dbname = 'ecommerceweb';
+
+// Database Username
+$dbuser = 'root';
+
+// Database Password
+$dbpass = 'root1234';
+
+// Defining base url
+define("BASE_URL", "");
+
+// Getting Admin url
+define("ADMIN_URL", BASE_URL . "admin" . "/");
+
+try {
+	$pdo = new PDO("mysql:host={$dbhost};dbname={$dbname}", $dbuser, $dbpass,
+	array( PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8" ));
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch( PDOException $exception ) {
+	echo "Connection error :" . $exception->getMessage();
+}
